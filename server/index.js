1f156795f3b31d201ffd4e78a3125149f251f5ed
Andrew Dodonov <nordhummer@gmail.com>
2021-12-06 22:24:41 +0300

    :bug: add proxy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const errorHandler = require('./middleware/errorHandlerMiddleware');
 const PORT = process.env.PORT || 5000 ;
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: ["http://localhost:3000"],
+      optionsSuccessStatus: 200
+    })
+  );
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
